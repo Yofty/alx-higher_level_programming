dab07ae38f8da8e502adc9938740f7a6cdd2f3af
Yofty <106748943+Yofty@users.noreply.github.com>
2022-09-19 10:34:14 -0700

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * list : is the linked list to be checked
+ * Return: int
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *x;
+
+	if (list == NULL)
+		return (0);
+	x = list->next;
+
+	while (x != NULL)
+	{
+		if (x == list)
+			return (1);
+		current = current->next;
+	}
+	return (0);
+}
