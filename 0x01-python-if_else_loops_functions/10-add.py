e0897b03bb8452a03027c443bb217185a950d7b2
Yofty <106748943+Yofty@users.noreply.github.com>
2022-09-20 12:48:00 -0700

    add

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add(a, b):
+    t = int(repr(a))
+    i = int(repr(b))
+    s = t + i
+    print("{}".format(s))
+    return s
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def print_last_digit(number):
     t = int(repr(number)[-1])
     print("{}".format(t), end="")
