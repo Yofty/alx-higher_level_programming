8cc8ed3c3808d2b06241cf214a8afa76d0b641a4
Yofty <106748943+Yofty@users.noreply.github.com>
2022-09-20 13:13:59 -0700

    last

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,10 +5,7 @@ def fizzbuzz():
         if x % 5 == 0 and x % 3 == 0:
             print("FizzBuzz", end=" ")
         elif x % 5 == 0:
-            if x != 100:
-                print("Buzz", end=" ")
-            else:
-                print("Buzz")
+            print("Buzz", end=" ")
         elif x % 3 == 0:
             print("Fizz", end=" ")
         else:
