13962841c5e2af780b10c27c51d4a4641fd3e5c4
Yofty <106748943+Yofty@users.noreply.github.com>
2022-09-29 12:17:21 -0700

    new

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    x = []
+    sum = 0
+    for num in my_list:
+        if num not in x:
+            sun += num
+            x.append(num)
+    return sum
