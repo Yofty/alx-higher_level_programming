ab8b89a670e13f75ecf79d4c3c0c61c7eadb7cb5
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-10 05:01:36 -0700

    raise exception

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    y = 0
+    for element in range(x):
+        try:
+            print("{:d}".format(my_list[element]), end="")
+            y += 1
+        except (ValueError, TypeError):
+            continue
+    print ("")
+    return y
