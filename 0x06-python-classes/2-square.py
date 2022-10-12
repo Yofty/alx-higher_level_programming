317ebf9e283deb9f1b86ac801acec017f87faba1
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-12 13:33:49 -0700

    correction

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,4 +16,4 @@ class Square:
         elif size < 0:
             aise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = size   #: size of the square
