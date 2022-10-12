ce7cdd8a7354a76f48cf7773e62fb1f847860caf
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-12 13:59:16 -0700

    access

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,7 +1,38 @@
 #!/usr/bin/python3
 """a class square that defines a square"""
+
+
 class Square:
     """private attribute"""
+
     def __init__(self, size=0):
-        """initiallize"""
+        """initiallize
+        Args:
+            value (int): size of the square.
+        """
         self.__size = size
+
+    @property
+    def size(self):
+        """int: private size.
+        Returns:
+            private size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be > 0")
+        else:
+            self.__size = value   #size of the square
+
+    def area(self):
+        """returns area
+        Returns:
+            area.
+        """
+        return self.__size**2
