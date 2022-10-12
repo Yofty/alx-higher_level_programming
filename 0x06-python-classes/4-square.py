d1f3f16e78cecc076b4f032922fe86ee7fcaf3a5
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-12 00:35:25 -0700

    correction

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a class square that defines a square"""
+class Square:
+    """private attribute"""
+    def __init__(self, size=0):
+        """initiallize"""
+        self.__size = size
