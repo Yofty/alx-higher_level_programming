2d70ab6bfa6dd8b43cd45a1d7c1ab93a96e11349
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-12 13:01:43 -0700

    second correction

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """a class Square that defines a square"""
+
+
 class Square:
     """a class Square that defines a square"""
-    def __int__(self, size):
+
+    def __init__(self, size):
         """initialize square"""
+
         self.__size = size #: size of the square
