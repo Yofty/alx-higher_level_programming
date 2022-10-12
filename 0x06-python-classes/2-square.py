7c2137bf09f999f70bbf6a9c1bb1500c0840018e
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-12 13:21:28 -0700

    correction 3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,14 +2,16 @@
 """a class Square that defines a square"""
 
 
-def __init__(self, size=0):
-    """initializes the size variable
-    instance atribute
+class Square:
+    """a class Square that defines a square"""
 
-    Args:
-        __size (int): The __size of new square. 
+    def __init__(self, size=0):
+        """initializes the size variable
+        instance atribute
 
-    """
+        Args:
+            __size (int): The __size of new square.
+        """
 
     self.__size = size
     if not isinstance(size, int):
