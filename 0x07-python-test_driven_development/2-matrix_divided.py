42d7390c314972947665648558439ef7b0b6e5b4
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-20 12:20:59 -0700

    last commit

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,8 +8,8 @@ def matrix_divided(matrix, div):
             not all(isinstance(row, list) for row in matrix) or
             not all((isinstance(ele, int) or isinstance(ele, float))
                 for ele in [num for row in matrix for num in row])):
-                raise TypeError("matrix must be a matrix (list of lists) of \
-                        integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
@@ -17,5 +17,5 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
 
     if div == 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError("division by zero ")
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
