fd44c2cf4f04931604a8d391dce612f98d92cde4
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-26 04:51:32 -0700

    corrfection

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,7 +6,7 @@ def pascal_triangle(n):
     """Represent Pascal's Triangle of size n."""
     if n <= 0:
         return []
-    
+
     triangles = [[1]]
     while len(triangles) != n:
         tri = triangles[-1]
