93435a197e9c05aada6f3efb78f7175c7f0cc795
Yofty <106748943+Yofty@users.noreply.github.com>
2022-10-30 04:51:18 -0700

    new

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,55 @@
+#!/usr/bin/pyhton3
+"""Defines a base model class."""
+import json
+import csv
+import turtle
+
+
+class Base:
+    """Represent the base model."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts."""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON serialization of a list of objects to a file."""
+         filename = cls.__name__ + ".json"
+         with open(filename, "w") as jsonfile:
+             if list_objs is None:
+                 jsonfile.write("[]")
+             else:
+                 list_dicts = [o.to_dictionary() for o in list_objs]
+                 jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string."""
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+     @classmethod
+     def create(cls, **dictionary):
+         """Return a class instantiated from a dictionary of attributes."""
+          if dictionary and dictionary != {}:
+              if cls.__name__ == "Rectangle":
+                  new = cls(1, 1)
+              else:
+                  new = cls(1)
+              new.update(**dictionary)
+              return new
