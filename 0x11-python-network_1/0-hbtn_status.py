cdf8303b14d7185541eed993a60377dbd77049e9
Yofty <106748943+Yofty@users.noreply.github.com>
2023-01-19 07:11:01 -0800

    documentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""A script that fetches https:intranet.htbn.io/status"""
+"""A script that 
+    - fetches https:intranet.htbn.io/status
+    - uses urlib package
+"""
 
 
 if __name__ == '__main__':
