a49cb5940d97ca95a2612251f2dcbb27d7e224bd
Yofty <106748943+Yofty@users.noreply.github.com>
2023-01-21 23:34:41 -0800

    correction 8, 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""takes your GitHub credentials and uses the GitHub API to display your id"""
+"""takes your GitHub credentials to display your id"""
 from sys import argv
 import requests
 
 
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/user',
-            auth=(argv[1], argv[2]))
+            auth = (argv[1], argv[2]))
     if r.status_code == 200:
         j = r.json()
         print(j.get('id'))
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if len(argv) == 2:
         arg = argv[1]
     try:
-        r = requests.post('http://0.0.0.0:5000/search_user', data={'q':arg})
+        r = requests.post('http://0.0.0.0:5000/search_user', data = {'q':arg})
         j = r.json()
         if j:
             print('[{}] {}'.format(j.get('id'), j.get('name')))
