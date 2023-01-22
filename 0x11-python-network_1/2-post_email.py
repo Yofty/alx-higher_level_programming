6f69a2cf43ec1433ddfe3ee701ba621cc623d654
Yofty <106748943+Yofty@users.noreply.github.com>
2023-01-21 22:39:35 -0800

    correction2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,7 +4,8 @@ import sys
 import urllib.parse
 import urllib.request
 
-if __name__ == "__min__":
+
+if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
