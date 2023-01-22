bf094617cec5a26e1878189d4c016cf9fecb831f
Yofty <106748943+Yofty@users.noreply.github.com>
2023-01-21 23:47:03 -0800

    bonus

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""prints the last 10 commits to git hub"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+            .format(argv[2], argv[1]))
+    l = r.json()
+    try:
+        for i in range(10):
+            print(l[i].get('sha'), l[i].get('commit')
+                    .get('author').get(name), sep=": ")
+    except:
+        pass
