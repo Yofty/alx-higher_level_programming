1b0a5d533ffd9022144d79911ccd4acd018faec5
Yofty <106748943+Yofty@users.noreply.github.com>
2023-01-21 22:32:04 -0800

    correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,14 +3,14 @@
     - fetches https:intranet.htbn.io/status
     - uses urlib package
 """
+import urllib.request
+import urllib.parse
 
 
 if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen(https://intranet.htbn.io/status') as res:
-        content = res.read()
+    with urllib.request.urlopen('https://intranet.htbn.io/status') as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:' , html.decode('utf-8'))
