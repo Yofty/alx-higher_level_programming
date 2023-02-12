3e92de102560cee43a35355072d7f10aff24f041
Yofty <106748943+Yofty@users.noreply.github.com>
2023-02-12 13:47:44 +0000

    1-type error

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,6 +5,6 @@ const file = process.argv[2];
 const string = process.argv[3];
 
 
-fs.WriteFile(file, string, 'utf-8', function (err) {
+fs.writeFile(file, string, 'utf-8', function (err) {
   if (err) console.log(err);
 });
