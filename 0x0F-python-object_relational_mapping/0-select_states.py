859e78a98eb81d8d2e3aad0a85d56dec9afb80ae
Yofty <106748943+Yofty@users.noreply.github.com>
2023-03-24 23:55:40 -0700

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""a script that lists all states from the database hbtn_0e_0_usa"""
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+usage: ./0-select_states.py <mysql username>\
+                            <mysql password>\
+                            <database name>
+"""
 import sys
 import MySQLdb
 
