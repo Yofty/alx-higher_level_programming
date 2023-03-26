1d7f7da00a507275b74bd306e4e422ff8d8ba024
Yofty <106748943+Yofty@users.noreply.github.com>
2023-03-26 09:14:54 -0700

    correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """
 lists all states from the database hbtn_0e_0_usa
-usage: ./0-select_states.py <mysql username>\
-                            <mysql password>\
-                            <database name>
 """
 import sys
 import MySQLdb
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MYSQLdb.connect(host="localhost",  user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], poer=3306)
     cur = db.cursor()
